4b1b3849b3e5934ad8489f21dafabb19471c2d34
Mahendra545 <mahendrasanebunny@gmail.com>
2022-07-04 19:50:27 +0530

    adding project

---
+++ trainingwithdbexample/src/main/java/com/trainingwithdbexample/TrainingwithdbexampleApplication.java
@@ -0,0 +1,13 @@
+package com.trainingwithdbexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingwithdbexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingwithdbexampleApplication.class, args);
+	}
+
+}
---
+++ trainingwithdbexample/src/test/java/com/trainingwithdbexample/TrainingwithdbexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainingwithdbexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingwithdbexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
